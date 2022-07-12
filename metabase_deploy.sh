e7274d11afd72c409a58d9d03858d8f42ca2ee04
marcellmueller <mlmueller@protonmail.com>
2022-07-12 15:17:33 -0700

    fix: deploy script helm directory

--- metabase_deploy.sh
+++ metabase_deploy.sh
@@ -4,7 +4,8 @@ set -euxo pipefail
 
 echo "Deploying metabase to openshift $*"
 
-cd helm/metabase
+cd helm/
 helm dep up
 helm upgrade --install --atomic -f ./values.yaml "$@" ccbc-metabase . \
   --debug --timeout=8m0s
+

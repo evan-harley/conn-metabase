04715ee54ff25733f699c5fdaaf19964dd9d3efc
marcellmueller <mlmueller@protonmail.com>
2022-10-17 11:35:16 -0700

    chore: use enterprise metabase image

--- metabase_deploy.sh
+++ metabase_deploy.sh
@@ -4,8 +4,6 @@ set -euxo pipefail
 
 echo "Deploying metabase to openshift $*"
 
-cd helm/
+cd helm
 helm dep up
-helm upgrade --install --atomic -f ./values.yaml "$@" ccbc-metabase . \
-  --debug --timeout=8m0s
-
+helm upgrade --install --atomic -f ./values.yaml "$@" ccbc-metabase . --timeout=8m0s

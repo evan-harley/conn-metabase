4dd9b58f5158e29bdf0a871eddbe2a92da5787ae
Rafael Solorzano <61289255+rafasdc@users.noreply.github.com>
2023-06-26 15:24:35 -0700

    fix: typo

--- .bin/metabase-deploy.sh
+++ .bin/metabase-deploy.sh
@@ -6,4 +6,4 @@ echo "Deploying metabase to openshift $*"
 
 cd helm
 helm dep up
-helm upgrade --install --atomic -f ./values.yaml --set livenessProbe.tcpSocket.port=3000 live --set livenessProbe.httpGet=null --set readinessProbe.httpGet=null "$@" ccbc-metabase . --timeout=8m0s
+helm upgrade --install --atomic -f ./values.yaml --set livenessProbe.tcpSocket.port=3000 --set livenessProbe.httpGet=null --set readinessProbe.httpGet=null "$@" ccbc-metabase . --timeout=8m0s
